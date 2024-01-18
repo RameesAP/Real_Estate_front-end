e45be9468c213d55eb6c460d0b145fe1438b351c
RameesAP <rameeswork@gmail.com>
2024-01-19 00:46:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/about"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/Signup"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
